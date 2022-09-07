4f1c0a4a789a1e6d8771aa0c093df7883f43c583
Mateo Altamirano <mateo.altamirano.1998@gmail.com>
2022-09-07 11:08:30 -0400

    style: initial plans ui

--- src/components/content/plans/plans.tsx
+++ src/components/content/plans/plans.tsx
@@ -2,8 +2,22 @@ import { Flex } from '@chakra-ui/react';
 
 const Plans = () => {
   return (
-    <Flex h={'100%'} w={'100%'}>
-      plans
+    <Flex
+      bgColor={'dark'}
+      borderRadius={'lg'}
+      flexDir={'column'}
+      h={'100%'}
+      p={'1.5rem'}
+      w={'100%'}
+    >
+      <Flex bgColor={'light'} h={'3rem'} w={'100%'} mb={'0.125rem'}></Flex>
+      <Flex
+        border={'1px solid'}
+        borderColor={'light'}
+        borderRadius={'lg'}
+        h={'100%'}
+        w={'100%'}
+      ></Flex>
     </Flex>
   );
 };
