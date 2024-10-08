9b8e316f67e32ab7633248b5aeb9aa2b2d6d5373
arnausd23 <arnausd23@hotmail.com>
2024-10-08 16:49:27 +0200

    Fix startApp.bat to use relative path for project directory

--- startApp.bat
+++ startApp.bat
@@ -5,7 +5,7 @@ REM Establece el directorio de trabajo a la ubicación del ejecutable
 SET "APP_DIR=%~dp0"
 
 REM Cambia al directorio donde está el proyecto de Next.js
-cd /d "C:\Users\HP\Documents\backup Connect\Connect-original"
+cd /d "%~dp0"
 
 REM Inicia el servidor de Next.js en segundo plano y deja el CMD visible pero no en primer plano
 start "" /b /min cmd.exe /c "npm run start"
