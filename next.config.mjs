0d421520c867f232396b723c7a7816dc1bd7a54b
arnausd23 <arnausd23@hotmail.com>
2024-10-13 20:26:25 +0200

    Refactor API routing to use '/connect/api' as the destination path

--- next.config.mjs
+++ next.config.mjs
@@ -29,7 +29,7 @@ export default defineNextConfig({
     return [
       {
         source: '/api/:path*',
-        destination: '/api/:path*',
+        destination: '/connect/api/:path*',
       },
     ];
   },
